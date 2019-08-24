d2fa7be37fd2d104deaf718364ad2ba630e8365c
Jbrn-2357 <53975911+Jbrn-2357@users.noreply.github.com>
2019-08-23 21:48:17 -0500

    Create observer.js

---
+++ observer.js
@@ -0,0 +1,98 @@
+/* 
+    Remind Bot
+   --------------
+    ~ since Remind doesn't have an api for reciveing messages I had to make all of this. Thank you Remind very cool.
+    ~ Takes an annoucement and sends it to discord. pretty simple.
+*/
+
+
+//required modules
+var chalk = require('chalk');
+const puppeteer = require('puppeteer');
+var Discord = require('discord.js');
+const token = 'BOT_TOKEN';
+const client = new Discord.Client();
+let annon = undefined;
+
+(observer = async () => {
+    client.login(token);
+
+    client.on('ready', async () => {
+
+        console.log(chalk.blue('[INFO] Observer starting...'));
+
+        let browser = await puppeteer.launch({
+            headless: false,
+            slowMo: 00
+        });
+
+        let page = await browser.newPage();
+
+        await page.goto(`https://www.remind.com/log_in`);
+        console.log(chalk.green('[SUCCESS] Observer Active!'));
+
+        let LatestDM = undefined;
+        let preAnnon = undefined;
+
+        setInterval(async () => {
+
+            latestDM = await page.evaluate(async () => {
+                if (document.getElementById('id-9') !== null) document.getElementById('id-9').value = "EMAIL_ADDRESS";
+                if (document.getElementById('id-10') !== null) document.getElementById('id-10').value = "PASSWORD";
+
+                getCircularReplacer = () => {
+                    const seen = new WeakSet();
+                    return (key, value) => {
+                        if (typeof value === "object" && value !== null) {
+                            if (seen.has(value)) return;
+                            seen.add(value);
+                        }
+                        return value;
+                    };
+                };
+
+                if (document.getElementById('id-9') !== null) {
+                    setTimeout(() => {
+                        document.body.getElementsByClassName("Button SignInForm-submit Button--primary Button--small Button--full")[0].click();
+                    }, 1000)
+                }
+
+                if (document.body.getElementsByClassName("Row Row--small") !== null) {
+                    setTimeout(() => {
+                        obj = { jsfy: undefined };
+                        current = undefined;
+                        previous = undefined;
+                        setInterval(() => {
+                            current = document.body.getElementsByClassName("Row Row--small")[0].children[0].children[0].children[0].children[1].children[1].children[0].firstChild.wholeText;
+
+                            if (current !== previous && current.startsWith(`Annou`)) {
+                                obj.jsfy = current;
+                            }
+                            previous = current;
+                        }, 500)
+                    }, 7000);
+                    if (typeof obj !== "undefined") return JSON.stringify(obj.jsfy, getCircularReplacer());
+                }
+            })
+            annon = latestDM;
+        }, 1000);
+
+        setInterval(() => {
+            if (typeof annon !== "undefined") {
+                if (annon !== preAnnon) {
+                    run = setInterval(sendAnnouncment);
+                }
+                preAnnon = annon;
+            }
+
+        }, 1000);
+
+        sendAnnouncment = () => {
+            client.channels.find(x => x.name === 'testing').send("@everyone" + "```" + annon.split("Announcement:")[1].split('"')[0] + "```");
+            console.log(chalk.blue('[INFO] Announcement Sent'));
+            clearInterval(run)
+        }
+
+    })
+})();
+
